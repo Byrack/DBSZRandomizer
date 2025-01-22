f13493be231289dd36f876c147240a17fd0b4c6d
Byrack <luddelennartsson@gmail.com>
2025-01-22 19:57:13 +0100

    Add project files.

---
+++ DBSZRandomizer/saga.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBSZRandomizer
+{
+    internal class saga
+    {
+        string Name;
+        bool IsChecked;
+
+        public saga(string name, bool isChecked) 
+        { 
+            Name = name;
+            IsChecked = isChecked;
+        }
+    }
+}
